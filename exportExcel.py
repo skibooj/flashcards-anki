789e32a62cbdb203a4b056585811c4a0fc232c31
skibooj <ssebastian156@gmail.com>
2018-12-20 21:31:42 +0100

    all trash removed

--- exportExcel.py
+++ exportExcel.py
@@ -2,24 +2,21 @@ import os
 import xlsxwriter
 
 
-def export_to_excel(file):
+def export_to_excel(dictionary):
     # set directory path
     os.chdir(r"/home/skibojq/projects/flashcards-anki/output")
     workbook = xlsxwriter.Workbook('out.xlsx')
     worksheet = workbook.add_worksheet('Flashcards')
 
-    # export to excel
-    dictionary = file
+    # exporting to excel
     row = 0
     col = 0
-    for key in dictionary.keys():
+    for key in dictionary:
         row += 1
         worksheet.write(row, col, key)
+        i = 0
         for item in dictionary[key]:
-            for i in range(1, 10):
-                try:
-                    worksheet.write(row, col + i, item[i - 1])
-                except:
-                    pass
+            worksheet.write(row, col + i, item)  # add all element from list
+            i += 1
 
     workbook.close()
--- flashcard.py
+++ flashcard.py
@@ -1,29 +1,31 @@
-import urllib.parse
 import requests
 
 
 def CreateFlashcard(word):
-    # setup api
-    main_api = 'https://googledictionaryapi.eu-gb.mybluemix.net/?'
-    define = word
-    url = main_api + urllib.parse.urlencode({'define': define}) + '&lang=en'
+    # setup request
+    url = 'https://googledictionaryapi.eu-gb.mybluemix.net/?define=' + word + '&lang=en'
     json_data = requests.get(url).json()
-    json_status = json_data['meaning']
 
-    # creating list
-    partOfSpeech = ['', 'noun', 'adverb', 'adjective', 'verb']
-    out = []
-    n = 0
+    flashcard = []  # create flashchard
+    try:
+        flashcard.append(json_data[0]['word'])  # add word
+    except:
+        pass
+    try:
+        flashcard.append(json_data[0]['phonetic'])  # add phonetic
+    except:
+        flashcard.append("#N/A")
+    try:
+        flashcard.append(list(json_data[0]['meaning'].keys())[0])  # add part of speech
+    except:
+        flashcard.append("#N/A")
+    try:
+        flashcard.append(list(json_data[0]['meaning'].values())[0][0]['definition'])  # add definition of word
+    except:
+        flashcard.append("#N/A")
+    try:
+        flashcard.append(list(json_data[0]['meaning'].values())[0][0]['example'])  # add example
+    except:
+        flashcard.append("#N/A")
 
-    for element in partOfSpeech:
-        out.append([element])
-        try:
-
-            out[n].append("definition: " + json_status[element][0]['definition'])
-            out[n].append('example: ' + json_status[element][0]['example'])
-
-        except:
-            pass
-        n += 1
-
-    return out
+    return flashcard
